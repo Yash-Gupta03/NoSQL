f5da9ff495e37676d53cf821e1db027994b74dbf
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-07-04 04:30:38 +0530

    mongodb data stored

---
+++ util/database.js
@@ -0,0 +1,25 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const MongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://yashgupta03:Yash123@project1.t7unbog.mongodb.net/?retryWrites=true&w=majority').then(client => {
+  console.log("connected");
+  _db = client.db();
+  callback();
+}).catch(error => {
+  console.log("Error");
+});
+}
+
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  console.log('error: not connected');
+
+}
+
+exports.MongoConnect = MongoConnect;
+exports.getDb = getDb;
