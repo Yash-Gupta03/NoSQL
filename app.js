38f339e95ba2110fb0fe5d05bef15bfef5114575
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-07-10 17:05:27 +0530

    user model created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoConnect = require('./util/database').MongoConnect;
+const User = require('./models/user');
 
 const errorController = require('./controllers/error');
 // const sequelize = require('./util/database');
@@ -25,13 +26,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findById(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.findById(1)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
