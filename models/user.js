b9b52f6cd54584d7ab0a541de9b3fdaef669c43d
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-07-10 18:00:45 +0530

    delete item from cart

--- models/user.js
+++ models/user.js
@@ -33,6 +33,30 @@ class User {
     return db.collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set:{cart: updatedCart}});
   }
 
+  getCart(){
+    const db = getDb();
+    const productIds = this.cart.items.map(i =>{
+      return i.productId;
+    })
+    return db.collection('products').find({_id: {$in: productIds}}).toArray().then(products => {
+      return products.map(p => {
+        return {...p, quantity: this.cart.items.find(i => {
+          return i.productId.toString() === p._id.toString();
+        }).quantity
+      }
+      })
+    });
+  }
+
+  deleteItemFromCart(productId){
+    const updateedCartItems = this.cart.items.filter(item => {
+      return item.productId !== productId.toString();
+    });
+    const db = getDb();
+    return db.collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set:{cart: {items:updateedCartItems}}});
+
+  }
+
   static findById(userId){
     const db = getDb();
     return db.collection('users').find({_id : new mongodb.ObjectId(userId)}).next();
