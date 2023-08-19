e00d3211133669307884f9d08dbbd5ad99041c81
Yash-Gupta03 <yashgupta.fast@gmail.com>
2023-08-19 10:29:41 +0530

    mongoose changes3

--- models/user.js
+++ models/user.js
@@ -15,6 +15,27 @@ const userSchema = new Schema({
   }
 })
 
+userSchema.methods.addToCart = function(product){
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+          return cp.productId.toString() === product._id.toString();
+        });
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+    
+        if (cartProductIndex >=0){
+          newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+          updateCartItems[cartProductIndex].quantity = newQuantity;
+        }else {
+          updatedCartItems.push({productId: new ObjectId(product._id), quantity: newQuantity})
+        }
+        
+        const updatedCart = {items: updatedCartItems};
+        const db = getDb();
+        this.cart = updatedCart;
+        return this.save();
+
+}
+
 module.exports = mongoose.model('user', userSchema);
 
 
